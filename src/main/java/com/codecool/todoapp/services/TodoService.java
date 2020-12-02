69fec3bbbc410825815911c8c4fa0d58c39e5b79
derdesz <dora.mailbox@gmail.com>
2020-12-02 16:39:57 +0100

    add deleteById, findAllByStatus, removeAllCompleted, findById service methods

--- src/main/java/com/codecool/todoapp/services/TodoService.java
+++ src/main/java/com/codecool/todoapp/services/TodoService.java
@@ -1,11 +1,13 @@
 package com.codecool.todoapp.services;
 
+import com.codecool.todoapp.model.Status;
 import com.codecool.todoapp.model.Todo;
 import com.codecool.todoapp.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -17,4 +19,22 @@ public class TodoService {
     public List<Todo> getAllTodos() {
         return todoRepository.findAll();
     }
+
+    public void deleteById(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+    public List<Todo> findAllByStatus(Status status) {
+        return todoRepository.findAllByStatus(status);
+    }
+
+    public void removeAllCompleted(){
+        todoRepository.removeAllByStatusEquals(Status.COMPLETE);
+    }
+
+    public Todo findById(Long id) {
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        return optionalTodo.orElse(null);
+    }
+
 }
